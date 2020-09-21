0bc451f54459a48aa207cf6e0bedff025112593b
msksaul <64514098+msksaul@users.noreply.github.com>
2020-09-21 14:59:37 -0600

    Initial Configuration

---
+++ src/firebase/index.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCOgV6iF8ugCEzo0wYYhvA-V4SLAPeyeZU",
+  authDomain: "whatsapp-clone-mern-4e9fa.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-mern-4e9fa.firebaseio.com",
+  projectId: "whatsapp-clone-mern-4e9fa",
+  storageBucket: "whatsapp-clone-mern-4e9fa.appspot.com",
+  messagingSenderId: "1052490925325",
+  appId: "1:1052490925325:web:64da7dc698820d14fe5c44",
+  measurementId: "G-VVR70K4DDB"
+};
